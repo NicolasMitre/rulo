4f8b20610c6f1f5f63d14fad999b2a8fc8f7f7d1
ACCIONETPC\Nicolás Mitre V1 <nicolasmelluso7@gmail.com>
2018-07-05 20:38:02 -0300

    nueva funcionalidad

--- js/rulo.js
+++ js/rulo.js
@@ -3,11 +3,13 @@ function cambia1()
 	var url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1571.8339406028306!2d-57.58631927533808!3d-38.00820649720849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584dece46acafb1%3A0xfb7ec5aaaf8a2879!2sLucciano&#39;s+F%C3%A1brica!5e0!3m2!1ses!2sar!4v1523901693573";
 	document.getElementById('google').src=url;
 }
+
 function cambia2()
 {
 	var url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3143.495512752369!2d-57.58089953147555!3d-38.01222612851394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2a201849dd878504!2sEscuela+de+Educaci%C3%B3n+T%C3%A9cnica+N%C2%B0+5!5e0!3m2!1ses!2sar!4v1523903990773"
 	document.getElementById('google').src=url;
 }
+
 function cambia3()
 {
 	var url = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12574.53498548436!2d-57.59291674999999!3d-38.00900255!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584dec51c47afef%3A0x92791be7ee62a49c!2sCervecer%C3%ADa+Antares!5e0!3m2!1ses!2sar!4v1523904147044"
@@ -16,7 +18,34 @@ function cambia3()
 
 // documento pedir
 
-function cambiar()
-{
-	alert("estamos trabajando en esta seccion");
-}
+var gustosDeHelado = [
+	{ 
+		nombre: "americana",
+		stock: 20
+	},
+	{ 
+		nombre: "frutilla",
+		stock: 15
+	},
+	{ 
+		nombre: "chocolate",
+		stock: 10
+	},
+	{ 
+		nombre: "banana split",
+		stock: 5
+	},
+	{ 
+		nombre: "kinotos al whisky",
+		stock: 1
+	}];
+
+	var list = document.getElementById("listaDeGustos");
+
+	for(var i = 0; i < gustosDeHelado.length; i++)
+	{
+		var nuevoLi = document.createElement("option");
+		nuevoLi.setAttibute("value", gustosDeHelado[i].nombre);
+		nuevoLi.textContent = gustosDeHelado[i].nombre;
+		list.appendChild(nuevoLi);
+	}
