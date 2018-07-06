34334a48b7f1b709fe0e002b34e34d68803183f6
ACCIONETPC\Nicolás Mitre V1 <nicolasmelluso7@gmail.com>
2018-07-05 21:13:31 -0300

    implementacion myFunction

--- js/rulo.js
+++ js/rulo.js
@@ -52,29 +52,8 @@ document.addEventListener('DOMContentLoaded', function(event) //carga el documen
 
 });
 
-/* <!DOCTYPE html>
-<html>
-<body>
+	function myFunction() {
+    var x = document.getElementById("listaDeGustos").value;
+    alert(x);
+	}
 
-<p>Select a new car from the list.</p>
-
-<select id="mySelect" onchange="myFunction()">
-  <option value="Audi">Audi
-  <option value="BMW">BMW
-  <option value="Mercedes">Mercedes
-  <option value="Volvo">Volvo
-</select>
-
-<p>When you select a new car, a function is triggered which outputs the value of the selected car.</p>
-
-<p id="demo"></p>
-
-<script>
-function myFunction() {
-    var x = document.getElementById("mySelect").value;
-    document.getElementById("demo").innerHTML = "You selected: " + x;
-}
-</script>
-
-</body>
-</html> */
